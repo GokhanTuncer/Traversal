0775ad37623a0625305457c2af944ffd1596d738
GokhanTuncer <gokhan_alphan@hotmail.com>
2024-11-11 18:29:56 +0300

    Add project files.

---
+++ TreversalCoreProje/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TreversalCoreProje.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
