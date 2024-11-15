50c7d048b12f874e069441993e53b4be07e0d0dc
GokhanTuncer <gokhan_alphan@hotmail.com>
2024-11-15 16:42:20 +0300

    Login ve Register sayfaları eklendi. LoginController eklendi Program.csde düzenlemeler

---
+++ TreversalCoreProje/Models/UserRegisterViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TreversalCoreProje.Models
+{
+	public class UserRegisterViewModel
+	{
+		[Required(ErrorMessage = "Lütfen Adınızı Giriniz")]
+        public string Name { get; set; }
+
+		[Required(ErrorMessage = "Lütfen Soyadınızı Giriniz")]
+		public string Surname { get; set; }
+
+		[Required(ErrorMessage = "Lütfen Kullanıcı Adınızı Giriniz")]
+		public string Username { get; set; }
+
+		[Required(ErrorMessage = "Lütfen Mail Adresinizi Giriniz")]
+		public string Email { get; set; }
+
+		[Required(ErrorMessage = "Lütfen Şifrenizi Giriniz")]
+		public string Password { get; set; }
+
+		[Required(ErrorMessage = "Lütfen Şifrenizi Tekrar Giriniz")]
+		[Compare("Password", ErrorMessage ="Şifreler uyumlu değil")]
+		public string ConfirmPassword { get; set; }
+	}
+}
