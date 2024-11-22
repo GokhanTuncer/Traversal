9783f57e8a2d73a5d8556b7382750710b67b84d1
GokhanTuncer <gokhan_alphan@hotmail.com>
2024-11-22 21:52:34 +0300

    Profildeki Platform Ayarları bölümü düzenlendi

---
+++ TreversalCoreProje/ViewComponents/MemberDashboard/_PlatformSetting.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TreversalCoreProje.ViewComponents.MemberDashboard
+{
+    public class _PlatformSetting : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+
+            return View();
+
+
+        }
+    }
+}
