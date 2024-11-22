caf8dc4ce2bf078eb84d4467d98d28e544517af3
GokhanTuncer <gokhan_alphan@hotmail.com>
2024-11-22 20:14:46 +0300

    Profildeki Rehber bölümü düzenlendi

--- EntityLayer/Concrete/Guide.cs
+++ EntityLayer/Concrete/Guide.cs
@@ -10,7 +10,6 @@ namespace EntityLayer.Concrete
     public class Guide
     {
         [Key]
-
         public int GuideID { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
---
+++ TreversalCoreProje/ViewComponents/MemberDashboard/_GuideList.cs
@@ -0,0 +1,22 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System.CodeDom;
+
+namespace TreversalCoreProje.ViewComponents.MemberDashboard
+{
+    public class _GuideList : ViewComponent
+    {
+        GuideManager guideManager = new GuideManager(new EfGuideDal());
+        public IViewComponentResult Invoke()
+        {
+            using (var context = new Context())
+            {
+                var values = guideManager.TGetList();
+                return View(values);
+            }
+
+        }
+    }
+}
