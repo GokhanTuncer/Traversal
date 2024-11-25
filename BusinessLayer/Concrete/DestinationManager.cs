f1937ce3c780ef482b551f0c0836d959a1f199fb
GokhanTuncer <gokhan_alphan@hotmail.com>
2024-11-25 14:26:27 +0300

    Admin dashboard'a  Destination crud işlemleri eklendi.

--- BusinessLayer/Concrete/DestinationManager.cs
+++ BusinessLayer/Concrete/DestinationManager.cs
@@ -1,7 +1,6 @@
 ﻿using BusinessLayer.Abstract;
 using DataAccessLayer.Abstract;
 using EntityLayer.Concrete;
-using Microsoft.EntityFrameworkCore.ChangeTracking;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,18 +20,28 @@ namespace BusinessLayer.Concrete
 
         public void TAdd(Destination t)
         {
-            throw new NotImplementedException();
+            _destinationDal.Insert(t);
         }
 
         public void TDelete(Destination t)
         {
-            throw new NotImplementedException();
+            _destinationDal.Delete(t);
         }
 
         public Destination TGetByID(int id)
         {
             return _destinationDal.GetByID(id);
         }
+
+        //public Destination TGetDestinationWithGuide(int id)
+        //{
+        //    return _destinationDal.GetDestinationWithGuide(id);
+        //}
+
+        //public List<Destination> TGetLast4Destinations()
+        //{
+        //    return _destinationDal.GetLast4Destinations();
+        //}
 
         public List<Destination> TGetList()
         {
@@ -41,7 +50,7 @@ namespace BusinessLayer.Concrete
 
         public void TUpdate(Destination t)
         {
-            throw new NotImplementedException();
+            _destinationDal.Update(t);
         }
     }
 }
