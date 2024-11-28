a5109f54ea1b58a4f5fa55620dca362e1b5622f0
GokhanTuncer <gokhan_alphan@hotmail.com>
2024-11-28 15:13:56 +0300

    Kullanıcı Yönetimi bölümü ve Container Dependency eklendi.

---
+++ BusinessLayer/Container/Extensions.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer.Abstract;
+using BusinessLayer.Concrete;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.EntityFramework;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Container
+{
+    public static class Extensions
+    {
+        public static void ContainerDependencies(this IServiceCollection builder)
+        {
+            builder.AddScoped<ICommentService, CommentManager>();
+            builder.AddScoped<ICommentDal, EfCommentDal>();
+            builder.AddScoped<IDestinationService, DestinationManager>();
+            builder.AddScoped<IDestinationDal, EfDestinationDal>();
+            builder.AddScoped<IAppUserService, AppUserManager>();
+            builder.AddScoped<IAppUserDal, EfAppUserDal>();
+        }
+    }
+}
--- TreversalCoreProje/Program.cs
+++ TreversalCoreProje/Program.cs
@@ -1,5 +1,6 @@
 using BusinessLayer.Abstract;
 using BusinessLayer.Concrete;
+using BusinessLayer.Container;
 using DataAccessLayer.Abstract;
 using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
@@ -7,6 +8,7 @@ using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Build.Execution;
+using Newtonsoft.Json.Linq;
 using TreversalCoreProje.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,8 +17,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<Context>();
 builder.Services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<Context>().AddErrorDescriber<CustomIdentityValidator>().AddEntityFrameworkStores<Context>();
-builder.Services.AddScoped<ICommentService, CommentManager>();
-builder.Services.AddScoped<ICommentDal, EfCommentDal>();
+
+builder.Services.ContainerDependencies();
+
 builder.Services.AddMvc(config =>
 {
     var policy = new AuthorizationPolicyBuilder()
