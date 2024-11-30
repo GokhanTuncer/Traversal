63cfdd4c8bc68345cbf74cb30d6c8877d3efb94d
GokhanTuncer <gokhan_alphan@hotmail.com>
2024-11-30 13:48:31 +0300

    Excel servisleri eklendi. Kontrolü düzenlendi

--- BusinessLayer/Container/Extensions.cs
+++ BusinessLayer/Container/Extensions.cs
@@ -30,6 +30,8 @@ namespace BusinessLayer.Container
             builder.AddScoped<IGuideService, GuideManager>();
             builder.AddScoped<IGuideDal, EfGuideDal>();
 
+            builder.AddScoped<IExcelService, ExcelManager>();
+
 
         }
     }
--- TreversalCoreProje/Controllers/ExcelController.cs
+++ TreversalCoreProje/Controllers/ExcelController.cs
@@ -1,4 +1,6 @@
-﻿using DataAccessLayer.Concrete;
+﻿using BusinessLayer.Abstract;
+using ClosedXML.Excel;
+using DataAccessLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 using OfficeOpenXml;
 using TreversalCoreProje.Models;
@@ -7,24 +9,15 @@ namespace TreversalCoreProje.Controllers
 {
     public class ExcelController : Controller
     {
+        private readonly IExcelService _excelService;
+
+        public ExcelController(IExcelService excelService)
+        {
+            _excelService = excelService;
+        }
         public IActionResult Index()
         {
-            ExcelPackage excel = new ExcelPackage();
-            var workSheet = excel.Workbook.Worksheets.Add("Sayfa1");
-            workSheet.Cells[1,1].Value = "Rota";
-            workSheet.Cells[1,2].Value = "Rehber";
-            workSheet.Cells[1,3].Value = "Kontenjan";
-
-            workSheet.Cells[2,1].Value = "Gürcistan Batum Turu";
-            workSheet.Cells[2,2].Value = "Kadir Yıldız";
-            workSheet.Cells[2,3].Value = "50";
-
-            workSheet.Cells[3,1].Value = "Sırbistan - Makedonya Turu";
-            workSheet.Cells[3,2].Value = "Zeynep Öztürk";
-            workSheet.Cells[3,3].Value = "35";
-
-            var bytes = excel.GetAsByteArray();
-            return File(bytes,"application/vnd.openxmlformats-officedocument.spreadsheethtml.sheet","dosya1.xlsx");
+            return View();
         }
         public List<DestinationModel> DestinationList()
         {
@@ -40,6 +33,40 @@ namespace TreversalCoreProje.Controllers
                 }).ToList();
             }
             return destinationModels;
+        }
+        public IActionResult StaticExcelReport()
+        {
+            return File(_excelService.ExcelList(DestinationList()), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "YeniExcel.xlsx");
+            //application/vnd.openxmlformats-officedocument.spreadsheetml.sheet
+        }
+
+        public IActionResult DestinationExcelReport()
+        {
+            using(var workBook = new XLWorkbook())
+            {
+                var workSheet = workBook.Worksheets.Add("Tur Listesi");
+                workSheet.Cell(1, 1).Value = "Şehir";
+                workSheet.Cell(1, 2).Value = "Konaklama Süresi";
+                workSheet.Cell(1, 3).Value = "Fiyat";
+                workSheet.Cell(1, 4).Value = "Kapasite";
+
+                int rowCount = 2;
+                foreach (var item in DestinationList())
+                {
+                    workSheet.Cell(rowCount, 1).Value = item.City;
+                    workSheet.Cell(rowCount, 2).Value = item.DayNight;
+                    workSheet.Cell(rowCount, 3).Value = item.Price;
+                    workSheet.Cell(rowCount, 4).Value = item.Capacity;
+                    rowCount++;
+                }
+
+                using (var stream = new MemoryStream())
+                {
+                    workBook.SaveAs(stream);
+                    var content = stream.ToArray();
+                    return File(content, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "YeniListe.xlsx");
+                }
+            }
         }
     }
 }
