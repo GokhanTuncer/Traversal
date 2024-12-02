89461cf43820c0b31cf8381a6156307fb338e84e
GokhanTuncer <gokhan_alphan@hotmail.com>
2024-12-02 19:59:36 +0300

    Destination ajax list devam

--- TreversalCoreProje/Areas/Admin/Controllers/CityController.cs
+++ TreversalCoreProje/Areas/Admin/Controllers/CityController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using TreversalCoreProje.Models;
 
@@ -7,6 +9,12 @@ namespace TreversalCoreProje.Areas.Admin.Controllers
     [Area("Admin")]
     public class CityController : Controller
     {
+        private readonly IDestinationService _destinationService;
+
+        public CityController(IDestinationService destinationService)
+        {
+            _destinationService = destinationService;
+        }
         public IActionResult Index()
         {
             return View();
@@ -14,31 +22,44 @@ namespace TreversalCoreProje.Areas.Admin.Controllers
 
         public IActionResult CityList()
         {
-            var jsonCity = JsonConvert.SerializeObject(cities);
+            var jsonCity = JsonConvert.SerializeObject(_destinationService.TGetList());
             return Json(jsonCity);
         }
-
-        public static List<CityClass> cities = new List<CityClass>
+        [HttpPost]
+        public IActionResult AddCityDestination(Destination destination)
         {
-            new CityClass
-            {
-            CityID = 1,
-            CityName = "Üsküp",
-            CityCountry ="Makedonya"
-            },
-            new CityClass
-            {
-            CityID = 2,
-            CityName = "Roma",
-            CityCountry ="İtalya"
-            },
+            _destinationService.TAdd(destination);
+            var values = JsonConvert.SerializeObject(destination);
+            return Json(values);
+        }
+		public IActionResult GetById(int id)
+		{
+			var values = _destinationService.TGetByID(id);
+			var jsonValues = JsonConvert.SerializeObject(values);
+			return Json(jsonValues);
+		}
 
-            new CityClass
-            {
-            CityID = 3,
-            CityName = "Londra",
-            CityCountry ="İngiltere"
-            }
-        };
-    }
+		//public static List<CityClass> cities = new List<CityClass>
+		//{
+		//    new CityClass
+		//    {
+		//    CityID = 1,
+		//    CityName = "Üsküp",
+		//    CityCountry ="Makedonya"
+		//    },
+		//    new CityClass
+		//    {
+		//    CityID = 2,
+		//    CityName = "Roma",
+		//    CityCountry ="İtalya"
+		//    },
+
+		//    new CityClass
+		//    {
+		//    CityID = 3,
+		//    CityName = "Londra",
+		//    CityCountry ="İngiltere"
+		//    }
+		//};
+	}
 }
