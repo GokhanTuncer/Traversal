a6a7428266531915e182966401e2ce5ce84f074a
GokhanTuncer <gokhan_alphan@hotmail.com>
2024-12-24 17:29:02 +0300

    Announcement Liste dtoları eklendi

--- TreversalCoreProje/Program.cs
+++ TreversalCoreProje/Program.cs
@@ -22,7 +22,11 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<Context>();
 builder.Services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<Context>().AddErrorDescriber<CustomIdentityValidator>().AddEntityFrameworkStores<Context>();
 builder.Services.AddAutoMapper(typeof(StartupBase));
+
 builder.Services.AddTransient<IValidator<AnnouncementAddDTOs>,AnnouncementValidator>();
+
+
+
 builder.Services.AddLogging(x =>
 {
     x.ClearProviders();
