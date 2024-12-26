61b23adeb768c1354ce6343c19eb5061c4565e9c
GokhanTuncer <gokhan_alphan@hotmail.com>
2024-12-26 18:01:06 +0300

    Ek: Film API'si eklendi

---
+++ TreversalCoreProje/Areas/Admin/Models/ApiMovieViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace TreversalCoreProje.Areas.Admin.Models
+{
+    public class ApiMovieViewModel
+    {
+        public int rank { get; set; }
+        public string title { get; set; }
+        public string rating { get; set; }
+        public int year { get; set; }
+        public int trailer { get; set; }
+    }
+}
