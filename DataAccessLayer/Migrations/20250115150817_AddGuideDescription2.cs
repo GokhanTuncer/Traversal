0b4fe779618e0e412fc74f167af0e14253b81680
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-01-15 18:13:32 +0300

    Guide'a description2 bölümü eklendi

---
+++ DataAccessLayer/Migrations/20250115150817_AddGuideDescription2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGuideDescription2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description2",
+                table: "Guide",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Date",
+                table: "Destination",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description2",
+                table: "Guide");
+
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "Destination");
+        }
+    }
+}
