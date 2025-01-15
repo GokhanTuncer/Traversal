99f2e62c7cd6729acbd2db25ddf0061f7ed8b246
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-01-15 15:13:57 +0300

    Yorum bölümündeki yorum sayacı eklendi, yorum tarihi düzenlendi, kullanıcı resmi düzenlendi

--- TreversalCoreProje/ViewComponents/Comments/_CommentList.cs
+++ TreversalCoreProje/ViewComponents/Comments/_CommentList.cs
@@ -1,4 +1,5 @@
 ﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Identity.Client;
@@ -8,9 +9,11 @@ namespace TreversalCoreProje.ViewComponents.Comments
     public class _CommentList:ViewComponent
     {
         CommentManager commentManager = new CommentManager(new EfCommentDal());
+        Context context=new Context();
         public IViewComponentResult Invoke(int id)
         {
-            var values = commentManager.TGetDestinationById(id);             
+            ViewBag.commentCount = context.Comment.Where(x=>x.DestinationID==id).Count();    
+            var values = commentManager.TGetListCommentWithDestinationAndUser(id);             
             return View(values);
         }
     }
