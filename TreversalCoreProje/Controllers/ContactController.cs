11385a101ffab229fe91102d87582abdbf73b216
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-01-15 18:36:55 +0300

    Contact Us bölümü için controller eklendi

---
+++ TreversalCoreProje/Controllers/ContactController.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TreversalCoreProje.Controllers
+{
+    [AllowAnonymous]
+    public class ContactController : Controller
+    {
+        ContactUsManager commentManager = new ContactUsManager(new EfContactUsDal());
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
