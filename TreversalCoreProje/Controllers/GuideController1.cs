d41f5012a9a32b1dfa98318a2bc3d21b78992c37
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-01-15 18:33:46 +0300

    Rehber bölümü eklendi

---
+++ TreversalCoreProje/Controllers/GuideController1.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TreversalCoreProje.Controllers
+{
+    [AllowAnonymous]
+    public class GuideController1 : Controller
+    {
+        GuideManager guideManager = new GuideManager(new EfGuideDal());
+        public IActionResult Index()
+        {
+            var values = guideManager.TGetList();
+            return View(values);
+        }
+    }
+}
