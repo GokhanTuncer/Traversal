3e701d281936a33914181fa30df127b1a4d627f9
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-01-16 12:56:52 +0300

    Contact Us sayfası index eklenmesi ve controller düzenlemesi

--- TreversalCoreProje/Controllers/ContactController.cs
+++ TreversalCoreProje/Controllers/ContactController.cs
@@ -9,9 +9,16 @@ namespace TreversalCoreProje.Controllers
     public class ContactController : Controller
     {
         ContactUsManager commentManager = new ContactUsManager(new EfContactUsDal());
+
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
-    }
+		[HttpPost]
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
 }
