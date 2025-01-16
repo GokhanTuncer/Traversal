6f2fde29a87956f09e69f38f1ffcf199f4db7e00
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-01-16 14:22:34 +0300

    SignalR için Visitor sınıfı eklendi

---
+++ SignalRApi/DAL/Visitor.cs
@@ -0,0 +1,18 @@
+﻿namespace SignalRApi.DAL
+{
+	public enum ECity
+	{
+		Istanbul=1,
+		Ankara=2,
+		Izmir=3,
+		Bursa=4,
+		Antalya=5
+	}
+	public class Visitor
+	{
+        public int VisitorID { get; set; }
+        public ECity City { get; set; }
+        public int CityVisitCount { get; set; }
+        public DateTime VisitDate { get; set; }
+    }
+}
