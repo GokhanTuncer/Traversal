2093c1d58dd6209e4588cbc804f40dc134cdb420
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-01-16 14:50:36 +0300

    Context eklenmesi ve connectionstring kurulumu

--- SignalRApi/Program.cs
+++ SignalRApi/Program.cs
@@ -1,8 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using SignalRApi.DAL;
+
+
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
+//builder.Services.AddEntityFrameworkNpgsql().AddDbContext<Context>(opt =>
+//opt.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
