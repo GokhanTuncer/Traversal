fab22f814db59829ea5c3f968816319f25db88c7
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-01-17 13:44:27 +0300

    Visitor chart, hub ve servisleri

---
+++ SignalRApi/Hubs/VisitorHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SignalRApi.Model;
+
+namespace SignalRApi.Hubs
+{
+	public class VisitorHub : Hub
+	{
+		private readonly VisitotService _visitorService;
+
+		public VisitorHub(VisitotService visitotService)
+		{
+			_visitorService = visitotService;
+		}
+		public async Task GetVisitorList()
+		{
+			await Clients.All.SendAsync("GetVisitList","_visitorService")
+		}
+	}
+}
---
+++ SignalRApi/Model/VisitotService.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SignalRApi.DAL;
+
+namespace SignalRApi.Model
+{
+	public class VisitotService
+	{
+		private readonly Context _context;
+		private readonly IHubContext<>
+	}
+}
