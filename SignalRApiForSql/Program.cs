f25fe30f3d29ffec8138b0da655c7caeeedd693d
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-01-19 13:22:18 +0300

    Program'a db context stringi eklendi

--- SignalRApiForSql/Program.cs
+++ SignalRApiForSql/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using SignalRApiForSql.DAL;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<Context>(options =>
+{
+    options.UseSqlServer(builder.Configuration["DefaultConnection"]);
+});
 
 var app = builder.Build();
 
