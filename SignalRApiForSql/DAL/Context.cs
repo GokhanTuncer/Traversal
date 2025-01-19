1c95adaa7153af54442524f0d74e7564c5ee654e
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-01-19 13:12:32 +0300

    Context ve Visitor classları oluşturuldu

---
+++ SignalRApiForSql/DAL/Context.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SignalRApiForSql.DAL
+{
+
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+
+
+        }
+        public DbSet<Visitor> Visitors { get; set; }
+    }
+
+}
