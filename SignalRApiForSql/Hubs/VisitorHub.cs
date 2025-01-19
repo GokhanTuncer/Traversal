fb323816aae1e2b6f907b945933e19695612100a
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-01-19 15:40:44 +0300

    Visitor controller düzenlemesi, hub, chart, service taşınması

---
+++ SignalRApiForSql/Hubs/VisitorHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SignalRApiForSql.Models;
+
+namespace SignalRApiForSql.Hubs
+{
+    public class VisitorHub : Hub
+    {
+        private readonly VisitorService _visitorService;
+
+        public VisitorHub(VisitorService visitotService)
+        {
+            _visitorService = visitotService;
+        }
+        public async Task GetVisitorList()
+        {
+            await Clients.All.SendAsync("GetVisitList", _visitorService.GetVisitorChartList());
+        }
+    }
+}
