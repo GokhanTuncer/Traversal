6c4de0d64e6e35b9916d3080cc69b8ff04ff11aa
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-01-19 13:32:01 +0300

    visitor controller eklendi

---
+++ SignalRApiForSql/Controllers/VisitorController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRApiForSql.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VisitorController : ControllerBase
+    {
+    }
+}
