371d7b8390022dba4424f938fdb5c9d7ff8b9cf9
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-01-20 13:06:34 +0300

    Rol güncelleme sayfası eklendi controller düzenlendi

--- TreversalCoreProje/Areas/Admin/Controllers/RoleController.cs
+++ TreversalCoreProje/Areas/Admin/Controllers/RoleController.cs
@@ -5,29 +5,29 @@ using TreversalCoreProje.Areas.Admin.Models;
 
 namespace TreversalCoreProje.Areas.Admin.Controllers
 {
-    [Area("Admin")]
-    [Route("Admin/Role")]
-    public class RoleController : Controller
-    {
-        
-        private readonly RoleManager<AppRole> _roleManager;
+	[Area("Admin")]
+	[Route("Admin/Role")]
+	public class RoleController : Controller
+	{
 
-        public RoleController(RoleManager<AppRole> roleManager)
-        {
-            _roleManager = roleManager;
-        }
+		private readonly RoleManager<AppRole> _roleManager;
+
+		public RoleController(RoleManager<AppRole> roleManager)
+		{
+			_roleManager = roleManager;
+		}
 		[Route("Index")]
 		public IActionResult Index()
-        {
-            var values = _roleManager.Roles.ToList();
-            return View(values);
-        }
+		{
+			var values = _roleManager.Roles.ToList();
+			return View(values);
+		}
 
-        [HttpGet]
+		[HttpGet]
 		[Route("CreateRole")]
 		public IActionResult CreateRole()
 		{
-			
+
 			return View();
 		}
 
@@ -36,21 +36,42 @@ namespace TreversalCoreProje.Areas.Admin.Controllers
 
 		public async Task<IActionResult> CreateRole(CreateRoleViewModel createRoleViewModel)
 		{
-            AppRole role=new AppRole()
-            {
-                Name = createRoleViewModel.RoleName
-            };
+			AppRole role = new AppRole()
+			{
+				Name = createRoleViewModel.RoleName
+			};
 
-            var result =await _roleManager.CreateAsync(role);
+			var result = await _roleManager.CreateAsync(role);
 
-            if (result.Succeeded)
-            {
-                return RedirectToAction("Index");
-            }
-            else
-            {
-                return View();
-            }
+			if (result.Succeeded)
+			{
+				return RedirectToAction("Index");
+			}
+			else
+			{
+				return View();
+			}
+		}
+		[Route("DeleteRole/{id}")]
+		public async Task<IActionResult> DeleteRole(int id)
+		{
+			var value = _roleManager.Roles.FirstOrDefault(x => x.Id == id);
+			await _roleManager.DeleteAsync(value);
+
+			return RedirectToAction("Index");
+		}
+
+		[HttpGet]
+		[Route("DeleteRole/{id}")]
+		public IActionResult UpdateRole(int id)
+		{
+			var value= _roleManager.Roles.FirstOrDefault(x=>x.Id == id);
+			UpdateRoleViewModel updateRoleViewModel = new UpdateRoleViewModel()
+			{
+				RoleID = value.Id,
+				RoleName = value.Name
+			};
+			return View(updateRoleViewModel);
 		}
 	}
 }
