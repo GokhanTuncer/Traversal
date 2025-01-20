c2e7be4006f95c7be00ffc3b8265a53ac8e460a6
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-01-20 12:29:59 +0300

    Role bölümü index düzenlemesi 1

--- TreversalCoreProje/Areas/Admin/Controllers/RoleController.cs
+++ TreversalCoreProje/Areas/Admin/Controllers/RoleController.cs
@@ -4,9 +4,10 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace TreversalCoreProje.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class RoleController : Controller
     {
-
+        
         private readonly RoleManager<AppRole> _roleManager;
 
         public RoleController(RoleManager<AppRole> roleManager)
