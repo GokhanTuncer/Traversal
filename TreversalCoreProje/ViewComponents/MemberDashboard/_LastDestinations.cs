d9fd0ea9103c96cb82b8a2bf6bbc0905e4bf944f
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-01-20 15:01:04 +0300

    Düzenleme

--- TreversalCoreProje/ViewComponents/MemberDashboard/_LastDestinations.cs
+++ TreversalCoreProje/ViewComponents/MemberDashboard/_LastDestinations.cs
@@ -1,6 +1,21 @@
-﻿namespace TreversalCoreProje.ViewComponents.MemberDashboard
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TreversalCoreProje.ViewComponents.MemberDashboard
 {
-    public class _LastDestinations
+    public class _LastDestinations : ViewComponent
     {
+        private readonly IDestinationService _destinationService;
+
+        public _LastDestinations(IDestinationService destinationService)
+        {
+            _destinationService = destinationService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var values = _destinationService.TGetLast4Destinations();
+            return View(values);
+        }
     }
 }
