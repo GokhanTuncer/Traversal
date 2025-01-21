68ce1e8d48043ef492b3466839546f074326ebbe
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-01-21 15:48:42 +0300

    MemberDashboard düzenlemesi ve MemberStatistic ViewComponentitnin eklenmesi

---
+++ TreversalCoreProje/ViewComponents/MemberDashboard/_MemberStatistic.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TreversalCoreProje.ViewComponents.MemberDashboard
+{
+    public class _MemberStatistic : ViewComponent
+    {
+        public IViewComponentResult Invoke() { return View(); }
+    }
+}
