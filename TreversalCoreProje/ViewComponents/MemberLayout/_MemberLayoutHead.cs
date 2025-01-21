5ba85cd2549e3a8025f678fbe7d5877c3ca9553b
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-01-21 16:29:28 +0300

    Member layout Head kısmı viewcomp.'a taşındı

---
+++ TreversalCoreProje/ViewComponents/MemberLayout/_MemberLayoutHead.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TreversalCoreProje.ViewComponents.MemberLayout
+{
+    public class _MemberLayoutHead : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+            { return View(); }
+    }
+}
