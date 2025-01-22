037a30f3aa234edc477d072f0b9bca5e0c6f8680
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-01-22 15:58:55 +0300

    Tur Rotası Arama sayfası eklendi

--- TreversalCoreProje/Areas/Member/Controllers/DestinationController.cs
+++ TreversalCoreProje/Areas/Member/Controllers/DestinationController.cs
@@ -10,10 +10,20 @@ namespace TreversalCoreProje.Areas.Member.Controllers
     public class DestinationController : Controller
     {
         DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
-            public IActionResult Index()
+        public IActionResult Index()
         {
             var values = destinationManager.TGetList();
             return View(values);
+        }
+        public IActionResult GetCitiesSearchByName(string searchString)
+        {
+            ViewData["CurrentFilter"] = searchString;
+            var values = from x in destinationManager.TGetList() select x;
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                values = values.Where(y => y.City.Contains(searchString));
+            }
+            return View(values.ToList());
         }
     }
 }
