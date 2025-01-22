8aa1ffbb9d8f653bc57ba2926186fe89e44be8a0
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-01-22 13:43:34 +0300

    Member Layout düzenlemesi ve dillerin vc'ye ayrıması

---
+++ TreversalCoreProje/ViewComponents/MemberLayout/_MemberLayoutLanguages.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TreversalCoreProje.ViewComponents.MemberLayout
+{
+    public class _MemberLayoutLanguages : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
