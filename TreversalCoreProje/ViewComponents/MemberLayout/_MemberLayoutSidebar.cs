a21197b3548b0f739235800b06d41f5337208ea3
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-01-22 13:59:31 +0300

    member layout sidebar vc

---
+++ TreversalCoreProje/ViewComponents/MemberLayout/_MemberLayoutSidebar.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+
+namespace TreversalCoreProje.ViewComponents.MemberLayout
+{
+    public class _MemberLayoutSidebar :ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
