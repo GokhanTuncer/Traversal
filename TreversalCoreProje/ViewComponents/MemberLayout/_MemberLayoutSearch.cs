1726482f6e321bbc960fa5475907deaf8c166001
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-01-22 13:52:52 +0300

    Member layout search vc

---
+++ TreversalCoreProje/ViewComponents/MemberLayout/_MemberLayoutSearch.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TreversalCoreProje.ViewComponents.MemberLayout
+{
+    public class _MemberLayoutSearch : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
