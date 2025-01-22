15611d7bfe0a34e4b897e740a02c166b5b687d8e
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-01-22 14:07:49 +0300

    member layout footer vc

---
+++ TreversalCoreProje/ViewComponents/MemberLayout/_MemberLayoutFooter.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TreversalCoreProje.ViewComponents.MemberLayout
+{
+    public class _MemberLayoutFooter :ViewComponent
+    {
+        public IViewComponentResult Invoke() { return View(); }
+    }
+}
