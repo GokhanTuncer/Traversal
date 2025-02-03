31789820ed6f78ba73c06dfbec29d75a73e99c5b
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-02-03 16:38:37 +0300

    Hakkımızda Bölümü eklendi

---
+++ TreversalCoreProje/Controllers/AboutController.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TreversalCoreProje.Controllers
+{
+	public class AboutController : Controller
+	{
+		private readonly IGuideService _guideService;
+
+		public AboutController(IGuideService guideService)
+		{
+			_guideService = guideService;
+		}
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
